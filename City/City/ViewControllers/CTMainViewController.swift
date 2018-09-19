481000cb9c1b403406e4cbf48c1dbce36576c0d7
Midhun MP <midhunmp7@gmail.com>
2018-09-19 13:58:34 +0530

    + Organised the files in the project + Added JSON file to project + UI design started

---
+++ City/City/ViewControllers/CTMainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CTMainViewController.swift
+//  City
+//
+//  Created by Midhun on 19/09/18.
+//  Copyright © 2018 Midhun. All rights reserved.
+//  Represents the main view controller of the application
+//
+
+import UIKit
+
+// MARK:- IBOutlets & Properties
+class CTMainViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
